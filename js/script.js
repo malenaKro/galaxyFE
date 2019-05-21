f253825368f1767c9d6f34e8113f9912cdd7b497
malenakronberger <s1710456019@students.fh-hagenberg.at>
2019-05-21 10:36:21 +0200

    Changes in HTML, CSS and JS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,83 @@
 (function(){
 
-    'use strict';
+    const NUM_OF_PARTICLES = 200;
 
-    let stars = document.querySelector("#stars");
-    console.log(stars);
+
+    let canvas;
+    let ctx;
+    let particles = [];
+
+    canvas = document.querySelector('#stars');
+    ctx = canvas.getContext('2d');
+
+    //due to retina displays -> all values times 2
+    canvas.width = window.innerWidth * 2.5;
+    canvas.height = window.innerHeight * 2.5;
+
+    //one Particle
+    class Particle {
+        constructor (x, y, color = 'white'){
+            this.x = x;
+            this.y = y;
+            this.radius = Math.floor(Math.random() * (6 - 1) + 1);
+            this.color = color;
+
+            //this.vx = Math.random() - 0.5;
+            this.vy = Math.random() - 0.5;
+        }
+
+        draw(){
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+            ctx.fill();
+            //ctx.fillRect(this.x, this.y, 8, 8);
+        }
+        update(){
+            //this.x+=this.vx;
+            this.y+=this.vy;
+
+            //check out of bounds
+            if (this.x <= 0
+                || this.x >= canvas.width) {
+                this.vx *= -1;
+            }
+            if (this.y <= 0
+                || this.y >= canvas.height) {
+                this.vy *= -1;
+            }
+        }
+    }
+
+    //create particles
+    for ( let i = 0; i < NUM_OF_PARTICLES; i++ ){
+
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+
+        let luminance = Math.random() * (100 - 90 ) + 90;
+        luminance = Math.floor(luminance);
+        let color = `hsl(177, 100%, ${luminance}%)`;
+
+        let p = new Particle (x, y, color);
+        particles.push(p);
+    }
+
+    //called every loop
+    function draw(){
+
+        //clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        //update & draw
+        particles.forEach( p => {
+            p.update();
+            p.draw();
+        });
+
+        window.requestAnimationFrame(draw);
+    }
+
+    draw();
 
 }());
